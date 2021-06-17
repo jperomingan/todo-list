58b916b938199056791255b41dd9ac4da15223e1
jperomingan@startechup.com <jperomingan@startechup.com>
2021-06-17 18:24:52 +0800

    Fix error

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+require("./config/passport");
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+dotenv.config({ path: "./config/config.env" });
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 app.use(require("./routes/todo"));
+
+app.listen(PORT, console.log(`listening at ${PORT}`));
--- routes/todo.js
+++ routes/todo.js
@@ -1,44 +1,42 @@
 const router = require("express").Router();
 const Todo_model = require("../models/todo");
 
-// Get all the user todo's
-router.get("/add/todo", (req, res) => {
-  const { todo } = req.body;
-  const newTodo = new Todo_model({ todo, email_: req.user.email, done: "0" });
-  if (todo == "") {
-    res.redirect("/");
-  }
-  newTodo
-    .save()
-    .then(() => {
-      console.log("done");
+router
+  .get("/add/todo", (req, res) => {
+    const { todo } = req.body;
+    const newTodo = new Todo_model({ todo, email_: req.user.email, done: "0" });
+    if (todo == "") {
       res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
+    }
+    newTodo
+      .save()
+      .then(() => {
+        console.log("done");
+        res.redirect("/");
+      })
+      .catch((err) => console.log(err));
+  })
 
-// Method for deleting the todo.
-router.get("/delete/todo/:_id", (req, res) => {
-  const { _id } = req.params;
-  Todo_model.deleteOne({ _id })
-    .then(() => {
-      console.log("deleted");
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
+  .get("/delete/todo/:_id", (req, res) => {
+    const { _id } = req.params;
+    Todo_model.deleteOne({ _id })
+      .then(() => {
+        console.log("deleted");
+        res.redirect("/");
+      })
+      .catch((err) => console.log(err));
+  })
 
-// Method to mark todo as done.
-router.get("/update/todo/:_id", (req, res) => {
-  const { _id } = req.params;
-  const info = Todo_model.find();
-  console.log(info);
-  Todo_model.updateOne({ _id }, { done: "1" })
-    .then(() => {
-      console.log("deleted");
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
+  .get("/update/todo/:_id", (req, res) => {
+    const { _id } = req.params;
+    const info = Todo_model.find();
+    console.log(info);
+    Todo_model.updateOne({ _id }, { done: "1" })
+      .then(() => {
+        console.log("deleted");
+        res.redirect("/");
+      })
+      .catch((err) => console.log(err));
+  });
 
 module.exports = router;
